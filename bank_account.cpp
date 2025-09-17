4eba8f11f291845c76912d7b6da949768dff7e9d
Chance Crosson <chancecrosson@gmail.com>
2025-09-17 11:33:09 -0500

    Added partial definition for displayMenu function.

--- bank_account.cpp
+++ bank_account.cpp
@@ -27,7 +27,20 @@ bool isValidWithdrawal(double amount){
 }
 
 int displayMenu(){
+    int menuOption;
 
+    std::cout << "==========================================\n" 
+              << "        SIMPLE BANK ACCOUNT SYSTEM        \n"
+              << "==========================================\n"
+              << "1. Deposit Money\n"
+              << "2. Withdraw Money\n"
+              << "3. Check Balance\n"
+              << "4. Exit\n"
+              << "==========================================\n"
+              << "Enter your choice (1-4): ";
+    std::cin >> menuOption;
+    std::cout << "D: Entered " << menuOption << std::endl;
+    
 }
 
 double getBalance(){
