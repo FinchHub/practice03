e75bd05090986b1424756027ea20cef9a7579bed
Chance Crosson <chancecrosson@gmail.com>
2025-09-17 11:23:58 -0500

    Added empty functions, declarations, includes.

---
+++ bank_account.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+void saveBalance(const std::string&); 
+double loadBalance(const std::string&);
+bool isValidWithdrawal(double amount);
+int displayMenu();
+double getBalance();
+
+
+int main() {
+    
+}
+
+void saveBalance(const std::string& filename = "account.txt"){
+
+}
+
+double loadBalance(const std::string& filename = "account.txt"){
+
+}
+
+bool isValidWithdrawal(double amount){
+
+}
+
+int displayMenu(){
+
+}
+
+double getBalance(){
+
+}
